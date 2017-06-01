3ad1ec72f3b7abc57bf40bfcbfe3ac6a7869266f
augfranco <augfranco@icloud.com>
2017-06-01 16:38:25 -0300

    MAPA
    MAPA

--- mapa2.py
+++ mapa2.py
@@ -16,9 +16,15 @@ def mapa():
 	bordatorre = 10
 	cantotorre = 11
 	bordabaixo = 12
+	pedra = 13
+	arvore = 14
+	homens = 15
+	casa = 16
+	sangue = 17
+	carro = 18
 
 
-	terrenos = [CANTOESQUERDO, LADOESQ, GRASS,AGUA, LADODIR, CIMAGRAMA, BAIXOGRAMA,CANTODIREITO, CAMINHO, tilecastelo, bordatorre, cantotorre, bordabaixo]
+	terrenos = [CANTOESQUERDO, LADOESQ, GRASS,AGUA, LADODIR, CIMAGRAMA, BAIXOGRAMA,CANTODIREITO, CAMINHO, tilecastelo, bordatorre, cantotorre, bordabaixo, pedra, arvore, homens, casa, sangue, carro]
  
 	textures = {
 		CANTOESQUERDO: pygame.image.load('CANTOESQUERDO.png'),
@@ -33,7 +39,16 @@ def mapa():
 		tilecastelo: pygame.image.load('tilecastelo.png'),
 		bordatorre: pygame.image.load('bordatorre.png'),
 		cantotorre: pygame.image.load('cantotorre.png'),
-		bordabaixo: pygame.image.load('bordabaixo.png')
+		bordabaixo: pygame.image.load('bordabaixo.png'),
+		pedra: pygame.image.load('pedra.png'),
+		arvore: pygame.image.load('arvore.png'),
+		homens: pygame.image.load('homens.png'),
+		casa: pygame.image.load('casa.png'),
+		sangue: pygame.image.load('sangue.png'),
+		carro: pygame.image.load('carro.png'),
+
+
+
 
 
 
@@ -43,30 +58,25 @@ def mapa():
 	}
 
 	TILESIZE = 40
-	MAPWIDTH = 40
-	MAPHEIGHT = 20
+	MAPWIDTH = 30
+	MAPHEIGHT = 15
 
 	tilemap = [
-	[tilecastelo, tilecastelo, tilecastelo, bordatorre, GRASS,GRASS, GRASS, AGUA, AGUA, AGUA,AGUA, AGUA,AGUA, AGUA,AGUA, AGUA, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS],
-	[tilecastelo, tilecastelo, tilecastelo, bordatorre, GRASS,GRASS, GRASS, AGUA, AGUA, AGUA,AGUA, AGUA,AGUA, AGUA,AGUA, AGUA, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS],
-	[tilecastelo, tilecastelo, tilecastelo, bordatorre, GRASS,GRASS, GRASS, AGUA, AGUA, AGUA,AGUA, AGUA,AGUA, AGUA,AGUA, AGUA, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS],
-	[tilecastelo, tilecastelo, tilecastelo, bordatorre, GRASS,GRASS, GRASS, AGUA, AGUA, AGUA,AGUA, AGUA,AGUA, AGUA,AGUA, AGUA, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS],
-	[tilecastelo, tilecastelo, tilecastelo,bordatorre, CAMINHO,CAMINHO, CAMINHO, CAMINHO, CAMINHO, CAMINHO, CAMINHO,CAMINHO, CAMINHO, CAMINHO, CAMINHO,CAMINHO, CAMINHO,CAMINHO, CAMINHO, CAMINHO, CAMINHO, CAMINHO, CAMINHO,CAMINHO, CAMINHO, CAMINHO, CAMINHO, CAMINHO, CAMINHO,CAMINHO, CAMINHO, CAMINHO, CAMINHO, CAMINHO, CAMINHO,CAMINHO, CAMINHO, CAMINHO, CAMINHO, CAMINHO],
-	[tilecastelo, tilecastelo, tilecastelo, bordatorre, GRASS,GRASS, GRASS, AGUA, AGUA, AGUA,AGUA, AGUA,AGUA, AGUA,AGUA, AGUA, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS],
-	[tilecastelo, tilecastelo, tilecastelo, bordatorre, GRASS,GRASS, GRASS, AGUA, AGUA, AGUA,AGUA, AGUA,AGUA, AGUA,AGUA, AGUA, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS],
-	[tilecastelo, tilecastelo, tilecastelo, bordatorre, GRASS,GRASS, GRASS, AGUA, AGUA, AGUA,AGUA, AGUA,AGUA, AGUA,AGUA, AGUA, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS],
-	[tilecastelo, tilecastelo, tilecastelo, bordatorre, GRASS,GRASS, GRASS, AGUA, AGUA, AGUA,AGUA, AGUA,AGUA, AGUA,AGUA, AGUA, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS],
-	[tilecastelo, tilecastelo, tilecastelo, bordatorre, GRASS,GRASS, GRASS, AGUA, AGUA, AGUA,AGUA, AGUA,AGUA, AGUA,AGUA, AGUA, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS],
-	[tilecastelo, tilecastelo, tilecastelo, bordatorre, GRASS,GRASS, GRASS,AGUA, AGUA, AGUA,AGUA, AGUA,AGUA, AGUA,AGUA, AGUA, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS],
-	[tilecastelo, tilecastelo, tilecastelo, bordatorre, GRASS,GRASS, GRASS, AGUA, AGUA, AGUA,AGUA, AGUA,AGUA, AGUA,AGUA, AGUA, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS],
-	[tilecastelo, tilecastelo, tilecastelo, bordatorre, GRASS,GRASS, GRASS, AGUA, AGUA, AGUA,AGUA, AGUA, AGUA, AGUA,AGUA, AGUA, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS],
-	[bordabaixo, bordabaixo, bordabaixo, cantotorre, GRASS,GRASS, GRASS, AGUA, AGUA, AGUA,AGUA, AGUA, AGUA, AGUA,AGUA, AGUA, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS],
-	[GRASS, GRASS,GRASS, GRASS, GRASS,GRASS, GRASS, AGUA, AGUA,AGUA,AGUA, AGUA, AGUA, AGUA,AGUA, AGUA, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS],
-	[GRASS, GRASS,GRASS, GRASS, GRASS,GRASS, GRASS, AGUA, AGUA, AGUA,AGUA, AGUA, AGUA, AGUA, AGUA,AGUA, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS],
-	[GRASS, GRASS,GRASS, GRASS, GRASS,GRASS, GRASS, AGUA, AGUA, AGUA,AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA,AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA,AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA],
-	[GRASS, GRASS,GRASS, GRASS, GRASS,GRASS, GRASS, AGUA, AGUA, AGUA,AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA,AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA,AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA],
-	[GRASS, GRASS,GRASS, GRASS, GRASS,GRASS, GRASS, AGUA, AGUA, AGUA,AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA,AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA,AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA],
-	[GRASS, GRASS,GRASS, GRASS, GRASS,GRASS, GRASS, AGUA, AGUA, AGUA,AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA,AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA,AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA],
+	[tilecastelo, tilecastelo, tilecastelo, bordatorre, GRASS,pedra, pedra, pedra, arvore, arvore, arvore,arvore, sangue,GRASS,arvore, arvore, arvore, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS],
+	[tilecastelo, tilecastelo, tilecastelo, bordatorre, GRASS,GRASS, pedra, pedra,arvore, sangue,GRASS,GRASS, GRASS, pedra,GRASS, arvore, arvore, arvore, pedra, GRASS,GRASS, carro, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS],
+	[tilecastelo, tilecastelo, tilecastelo, bordatorre, GRASS,pedra, GRASS, GRASS,pedra, pedra,GRASS,GRASS, GRASS, GRASS,GRASS, arvore, arvore, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, sangue],
+	[tilecastelo, tilecastelo, tilecastelo, bordatorre, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS],
+	[tilecastelo, tilecastelo, tilecastelo,bordatorre, CAMINHO,CAMINHO, CAMINHO, CAMINHO, CAMINHO, CAMINHO, CAMINHO,CAMINHO, CAMINHO, CAMINHO, CAMINHO,CAMINHO, CAMINHO,CAMINHO, CAMINHO, CAMINHO, CAMINHO, CAMINHO, CAMINHO,CAMINHO, CAMINHO, CAMINHO, CAMINHO, CAMINHO, CAMINHO,CAMINHO],
+	[tilecastelo, tilecastelo, tilecastelo, bordatorre, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS],
+	[tilecastelo, tilecastelo, tilecastelo, bordatorre, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS,GRASS, arvore, arvore, GRASS, GRASS, GRASS, arvore, arvore, arvore, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, sangue, sangue],
+	[tilecastelo, tilecastelo, tilecastelo, bordatorre, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS,GRASS, pedra, pedra, arvore, pedra, pedra, arvore, arvore, arvore, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS,GRASS, GRASS, GRASS, GRASS, GRASS],
+	[tilecastelo, tilecastelo, tilecastelo, bordatorre, GRASS,GRASS, GRASS, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, AGUA, arvore, arvore, sangue, pedra, arvore, arvore,GRASS, pedra, pedra, pedra, arvore,arvore, GRASS, GRASS, arvore, arvore],
+	[tilecastelo, tilecastelo, tilecastelo, bordatorre, sangue,GRASS, GRASS, AGUA, AGUA, AGUA,AGUA, AGUA, AGUA, AGUA, GRASS, arvore, arvore, arvore, arvore, GRASS,GRASS, pedra, pedra, pedra, arvore,sangue, GRASS, arvore, arvore, arvore],
+	[tilecastelo, tilecastelo, tilecastelo, bordatorre, GRASS,GRASS, GRASS,AGUA, AGUA, GRASS,GRASS, GRASS, AGUA, AGUA, GRASS, GRASS, sangue, GRASS, GRASS, GRASS,GRASS, pedra, pedra, pedra, arvore,arvore, GRASS, arvore, arvore, GRASS],
+	[tilecastelo, tilecastelo, tilecastelo, bordatorre, GRASS,GRASS, GRASS, AGUA, AGUA, GRASS,GRASS, GRASS,AGUA, AGUA, AGUA, AGUA, GRASS, casa, GRASS, sangue,GRASS, GRASS, GRASS, pedra, pedra, pedra, arvore, arvore, arvore, arvore],
+	[tilecastelo, tilecastelo, tilecastelo, bordatorre, GRASS,GRASS, GRASS, AGUA, AGUA, GRASS,GRASS, GRASS, AGUA, AGUA, AGUA, AGUA, GRASS, casa, GRASS, casa,GRASS, GRASS,GRASS,pedra, pedra, pedra, pedra, pedra,  pedra, arvore],
+	[bordabaixo, bordabaixo, bordabaixo, cantotorre, GRASS,GRASS, GRASS, AGUA, AGUA, GRASS,GRASS, GRASS, GRASS, GRASS, AGUA, AGUA, GRASS, GRASS, GRASS, casa,GRASS, GRASS,GRASS, pedra, pedra,pedra, pedra, arvore, arvore, arvore],
+	[GRASS, GRASS,GRASS, GRASS, GRASS,GRASS, GRASS, AGUA, AGUA,GRASS,GRASS, GRASS, GRASS, GRASS, AGUA, AGUA, GRASS, GRASS, GRASS, GRASS,GRASS, sangue, GRASS, GRASS, GRASS,GRASS, pedra, pedra, arvore, arvore],
 
 	]
 
